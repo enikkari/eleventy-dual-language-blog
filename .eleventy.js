092ef10ce25a8807e0c40a3967e6b5f2715467ab
enikkari <eevanikkari@gmail.com>
2024-02-27 20:19:29 +0200

    improvements

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./css");
   eleventyConfig.addWatchTarget("./css/");
+  eleventyConfig.addPassthroughCopy("./img");
+  eleventyConfig.addWatchTarget("./img/");
   return {
     dir: {
       input: "src",
